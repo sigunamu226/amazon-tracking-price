77914003b8607cbfd19a537ed78092e0b6281ee2
sigunamu226 <sigunamu226@gmail.com>
2022-12-26 19:01:39 +0900

    定期実行処理の実装

--- items/apps.py
+++ items/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ItemsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'items'
+
+    def ready(self):
+        from items.tasks.task import start
+        start()
