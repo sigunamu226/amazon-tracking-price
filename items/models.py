28f08e616371896281ccb1ec59ed178c7cc4881c
sigunamu226 <sigunamu226@gmail.com>
2022-12-26 18:59:12 +0900

    モデルの整理

--- items/models.py
+++ items/models.py
@@ -17,7 +17,6 @@ class Item(models.Model):
         '商品画像', upload_to=IMAGE_DIR, db_index=True)
     now_price = models.CharField('現在の価格', max_length=50, db_index=True)
     hope_price = models.CharField('希望価格', max_length=50, db_index=True)
-    recent_average = models.CharField('直近の平均価格', max_length=50, db_index=True, null=True, blank=True)
     fluctuating_value = models.CharField('変動値', max_length=50, db_index=True, null=True, blank=True)
     url = models.CharField('URL', max_length=1000, db_index=True)
     updated_at = models.DateTimeField('更新日時', default=now)
@@ -39,7 +38,7 @@ class CacheItems(models.Model):
     created_at = models.DateTimeField('作成日時', default=now)
 
     class Meta:
-        verbose_name = '蓄積された値段'
+        verbose_name = '商品'
         verbose_name_plural = '値段蓄積マスタ'
 
     def __str__(self):
