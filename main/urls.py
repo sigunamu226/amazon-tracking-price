989669eac58dd971fba60d906924b863e3451b05
InoueHayato226 <inouehayato@rootteam.co.jp>
2023-05-21 18:51:24 +0900

    url周りを調整

--- main/urls.py
+++ main/urls.py
@@ -4,12 +4,12 @@ from accounts.views import AccountView
 from items.views import ItemsView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', AccountView.login, name='login'),
-    path('signup', AccountView.signup, name='signup'),
-    path('login', AccountView.login, name='login'),
-    path('logout', AccountView.logout, name="logout"),
-    path('items/', ItemsView.itemtable, name="items"),
-    path('items/regist', ItemsView.itemregist, name="items_regist"),
-    path('items/delete', ItemsView.itemdelete, name="items_delete")
+    path('am-tracking/admin/', admin.site.urls),
+    path('am-tracking', AccountView.login, name='login'),
+    path('am-tracking/signup', AccountView.signup, name='signup'),
+    path('am-tracking/login', AccountView.login, name='login'),
+    path('am-tracking/logout', AccountView.logout, name="logout"),
+    path('am-tracking/items/', ItemsView.itemtable, name="items"),
+    path('am-tracking/items/regist', ItemsView.itemregist, name="items_regist"),
+    path('am-tracking/items/delete', ItemsView.itemdelete, name="items_delete")
 ]
