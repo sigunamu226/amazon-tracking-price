71ff9b839253cdbdef0bd9af008fbd7ec1e658de
sigunamu226 <sigunamu226@gmail.com>
2023-06-18 20:05:18 +0900

    driver使用後はcloseをするように

--- items/service.py
+++ items/service.py
@@ -9,23 +9,29 @@ from PIL import Image
 import uuid
 from items.models import Item
 
+
 def getAmazonSoup(amazonURL):
-    firefox_servie = fs.Service(executable_path="/code/linux_gecko/geckodriver")
+    firefox_servie = fs.Service(
+        executable_path="/code/linux_gecko/geckodriver")
     option = Options()
     option.add_argument('--headless')
     driver = webdriver.Firefox(service=firefox_servie, options=option)
     driver.get(amazonURL)
     html = driver.page_source.encode('utf-8')
     soup = BeautifulSoup(html, "html.parser")
+    driver.close()
     return soup
+
 
 def registerItem(amazonURL, user, hopePrice):
     soup = getAmazonSoup(amazonURL)
 
-    nowPrice = soup.find("span", class_="a-price-whole").get_text().replace(",","")
+    nowPrice = soup.find(
+        "span", class_="a-price-whole").get_text().replace(",", "")
     imageName = str(uuid.uuid4())[-6:]
     itemName = soup.find(id="productTitle").get_text().strip()
-    itemImage = soup.find(id="imgTagWrapperId").find("img", alt=itemName).get("src")
+    itemImage = soup.find(id="imgTagWrapperId").find(
+        "img", alt=itemName).get("src")
     re = requests.get(itemImage)
     img_file = io.BytesIO(re.content)
     img_open = Image.open(img_file)
@@ -34,13 +40,17 @@ def registerItem(amazonURL, user, hopePrice):
 
 
 def savePrice(user, itemName, imageName, nowPrice, hopePrice, amazonURL):
-    item = Item(user=user, name=itemName, image='templates/static/upload_to/' + imageName + '.png', now_price=nowPrice, hope_price=hopePrice, url=amazonURL)
+    item = Item(user=user, name=itemName, image='templates/static/upload_to/' +
+                imageName + '.png', now_price=nowPrice, hope_price=hopePrice, url=amazonURL)
     item.save()
+
 
 def periodicUpdate(amazonURL):
     soup = getAmazonSoup(amazonURL)
-    nowPrice = soup.find("span", class_="a-price-whole").get_text().replace(",","")
+    nowPrice = soup.find(
+        "span", class_="a-price-whole").get_text().replace(",", "")
     return nowPrice
+
 
 def deleteItem(itemId):
     deleteItem = Item.objects.get(id=itemId)
